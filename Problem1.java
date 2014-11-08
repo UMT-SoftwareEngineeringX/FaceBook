4f24595ac6cccbd26acc6d48a531db0e609c27a0
kkhurramshahzad <khumimks@gmail.com>
2014-11-07 19:56:30 -0800

    RightAngle Triangle

---
+++ Problem1.java
@@ -0,0 +1,41 @@
+package fundamentals;
+
+import java.util.Scanner;
+public class Problem1 {
+	
+	// find either it is right angle triangle or not 
+
+	public static void main(String[] args) {
+		
+		Scanner getinput=new Scanner(System.in);
+		
+		double h,b,p;
+		double Sum_of_b_and_p;
+		
+		System.out.println("Enter the value for hypotenuse ");
+		h=getinput.nextDouble();
+		
+		System.out.println("Enter the value for breadth ");
+		b=getinput.nextDouble();
+
+		System.out.println("Enter the value for perpendicular ");
+		p=getinput.nextDouble();
+		
+		h = h*h;	b = b*b;	p = p*p;
+		
+		 Sum_of_b_and_p = b+p;
+		 
+		 if(h== Sum_of_b_and_p){
+			 System.out.println("Since "+h +" = " +Sum_of_b_and_p +" therefore ");
+			 System.out.println("It is right angle triangle ");
+		 }
+		 else{
+			 System.out.println("Since "+h +" != " +Sum_of_b_and_p +" therefore ");
+			 System.out.println("It is not right angle triangle ");
+		 }
+		
+
+
+	}
+
+}
