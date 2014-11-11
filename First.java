d33d22f9d05439f9d4aebc44e862ca4ed2a00454
SyedMushahid <hamdaniideal@outlook.com>
2014-11-11 00:34:12 -0600

    Create First.java

---
+++ First.java
@@ -0,0 +1,13 @@
+public class First {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Syed Mushahid!!");
+        System.out.println("12003065284");
+        System.out.println("BS-SE");
+        System.out.println("A-");
+        System.out.println("+923337167076");
+    }
+    
+}
